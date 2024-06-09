2f7444b872af28d246aee691e2969f401468160e
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-09 21:30:14 +0200

    new navbar attempt

---
+++ files/script.js
@@ -0,0 +1,23 @@
+// script.js
+$(document).ready(function() {
+    // Event listener for close button
+    $('#closeBtn').click(function() {
+      closeNav();
+    });
+  
+    // Event listener for open button
+    $('#openBtn').click(function() {
+      openNav();
+    });
+  });
+  
+  // Function to open navigation
+  function openNav() {
+    $('#myNav').css('height', '100%');
+  }
+  
+  // Function to close navigation
+  function closeNav() {
+    $('#myNav').css('height', '0');
+  }
+  
