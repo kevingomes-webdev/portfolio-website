4eb78b670f1b34a5364630f95f07c27007158988
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-10 17:42:28 +0200

    links

--- files/script.js
+++ files/script.js
@@ -20,4 +20,27 @@ $(document).ready(function() {
   function closeNav() {
     $('#myNav').css('height', '0');
   }
+
+
+// Horizontal Scrolling Animation
+
+document.addEventListener("DOMContentLoaded", function() {
+  const container = document.querySelector(".races");
+  const sections = gsap.utils.toArray(".races .scrollBoxes");
+
+  let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".racesWrapper",
+      pin: true,
+      scrub: 1,
+      end: () => "+=" + container.offsetWidth,
+      markers: true,
+    }
+  });
+
+  console.log(1 / (sections.length - 1));
+});
+
   
