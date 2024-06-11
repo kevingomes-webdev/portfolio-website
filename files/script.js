76ca1b5e7827cb17e253e2119b090c22ed8c3459
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-11 22:15:32 +0200

    removed animation

--- files/script.js
+++ files/script.js
@@ -22,42 +22,8 @@ $(document).ready(function() {
   }
 
 
-// Horizontal Scrolling Animation
 
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  const container = document.querySelector(".scrollContainer");
-  
-  function getScrollAmount() {
-    return container.scrollWidth - window.innerWidth;
-  }
-
-  function createScrollAnimation() {
-    // Kill any existing ScrollTriggers to prevent multiple instances
-    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-
-    // Create the tween animation
-    gsap.to(container, {
-      x: () => -getScrollAmount(),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".scrollWrapper",
-        pin: true,
-        scrub: 1,
-        end: () => "+=" + getScrollAmount(),
-        invalidateOnRefresh: true,
-        markers: true,
-      }
-    });
-  }
-
-  // Initial animation creation
-  createScrollAnimation();
-
-  // Re-create the animation on window resize
-  window.addEventListener('resize', createScrollAnimation);
-});
 
 
 
