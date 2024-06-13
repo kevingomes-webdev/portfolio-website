1c530e8a2bd70b17cbaf699ccf243bc4efa27c64
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-14 00:35:36 +0200

    link change

--- index/script.js
+++ index/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#submit-btn').click(function() {
         var password = $('#password').val();
-        var correctPassword = "yourpassword"; // Replace with your desired password
+        var correctPassword = "password"; // Replace with your desired password
         if (password === correctPassword) {
-            window.location.href = "resume.html"; // Replace with the path to your resume
+            window.location.href = "kevingomes-webdev.github.io/portfolio-website/resume/resume.html"; // Replace with the path to your resume
         } else {
             $('#error-message').text("Incorrect password. Please try again.");
         }
