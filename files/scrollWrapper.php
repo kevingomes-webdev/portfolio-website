c5fbbf9e5c0ab76374530682a974870331979d62
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-13 16:03:36 +0200

    animation

---
+++ files/scrollWrapper.php
@@ -0,0 +1,44 @@
+<div class="racesWrapper">
+  <div class="races">
+    <div class="scrollBoxes">
+      <p class="h7">2024</p>
+      <p class="h8">January</p>
+      <p class="h9">Photo Editing</p>
+      <p>
+        I recently started my photo editing journey.
+      </p>
+    </div>
+    <div class="scrollBoxes">
+      <p class="h7">2024</p>
+      <p class="h8">January</p>
+      <p class="h9">Photo Editing</p>
+      <p>
+        I recently started my photo editing journey.
+      </p>
+    </div>
+    <div class="scrollBoxes">
+      <p class="h7">2024</p>
+      <p class="h8">January</p>
+      <p class="h9">Photo Editing</p>
+      <p>
+        I recently started my photo editing journey.
+      </p>
+    </div>
+    <div class="scrollBoxes">
+      <p class="h7">2024</p>
+      <p class="h8">January</p>
+      <p class="h9">Photo Editing</p>
+      <p>
+        I recently started my photo editing journey.
+      </p>
+    </div>
+    <div class="scrollBoxes">
+      <p class="h7">2024</p>
+      <p class="h8">January</p>
+      <p class="h9">Photo Editing</p>
+      <p>
+        I recently started my photo editing journey.
+      </p>
+    </div>
+  </div>
+</div>
