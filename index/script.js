58bfe412684c40e20b5dcb38dd578337538ff867
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-15 01:15:59 +0200

    changes

--- index/script.js
+++ index/script.js
@@ -8,15 +8,59 @@ $(document).ready(function() {
             window.location.href = "kevingomes-webdev.github.io/portfolio-website/resume/resume.html"; // Replace with the path to your resume
         } else {
             $('#error-message').text("Incorrect password. Please try again.");
+            errorMessage();
         }
     });
+
+    function playAnimation() {
+        const tl = gsap.timeline();
+
+        tl.from(".containerElements > *", {
+            y: "200%",
+            opacity: 0,
+            duration: 3,
+            ease: "power4.out",
+            stagger: 0.3
+        })
+    }
+
+    playAnimation();
+
+    function errorMessage() {
+        const em = gsap.timeline();
+
+            // Slide up and fade in
+            em.to("#error-message", {
+                y: 0,
+                opacity: 1,
+                duration: 0.3,
+                ease: "power1.out"
+            });
+
+            // Scale up slightly
+            em.to("#error-message", {
+                duration: 0.3,
+                opacity: 0,
+                ease: "power1.out"
+            });
+
+            em.to("#error-message", {
+                duration: 0.3,
+                opacity: 1,
+                ease: "power1.out"
+            });
+    }
+
+    
 });
 
 
 /***Animation****/
 
-const tl = gsap.timeline();
 
-tl.to(".box", {x: 500, duration: 5})
-  .to(".box", {y: 200, duration: 3})
-  .to(".box", {x: 0, duration: 2})
+
+
+
+
+
+
