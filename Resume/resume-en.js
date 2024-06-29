859358b8e0a9582e9777084db94321da1f99befb
kevingomes-webdev <154997714+kevingomes-webdev@users.noreply.github.com>
2024-06-29 22:32:05 +0200

    changes

--- Resume/resume-en.js
+++ Resume/resume-en.js
@@ -21,11 +21,55 @@ $(document).ready(function() {
     $('#myNav').css('height', '0');
   }
 
+//ANIMATIONS
+
+  // Hover Animation
+
+$(document).ready(function() {
+  // Select all grid boxes
+  var $boxes = $('.images-100');
+
+  // Iterate through each box to create individual hover animations
+  $boxes.each(function() {
+    var $box = $(this);
+
+    // Create the hover animation using GSAP timeline for each box
+    var hoverAnimation = gsap.timeline({ paused: true })
+      .fromTo($box, { scale: 0.95}, { scale: 1 });
+
+    // Add event listeners for hover using jQuery
+    $box.hover(
+      function() {
+        hoverAnimation.play();
+      },
+      function() {
+        hoverAnimation.reverse();
+      }
+    );
+  });
+});
 
 
+// Smooth Scrolling
 
+const ss = gsap.timeline({
+  scrollTrigger: {
+    trigger: "body",
+    start: "top top",
+    end: "+=200%", // Adjust based on how far you want the scrolling animation to go
+    scrub: 1 // Smoothly animates the timeline
+  }
+});
 
+// Define animations for each section
+ss.to(window, { scrollTo: { y: "section", autoKill: false }, duration: 1 });
 
-
-
-  
+// Create ScrollTrigger to control animations
+ScrollTrigger.create({
+  animation: ss,
+  trigger: "body",
+  start: "top top",
+  end: "+=200%", // Adjust based on how far you want the scrolling animation to go
+  scrub: 1, // Smoothly animates the timeline
+  pin: true // Pins the animation so it stays visible while scrolling
+});
